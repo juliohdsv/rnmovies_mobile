546d8751a70216125b610329d29a71e2902db96c
juliohdsv <ricksv92@gmail.com>
2024-11-08 07:08:40 -0300

    feat(App): created flatlist

--- App.js
+++ App.js
@@ -1,30 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
 
   const [feed, setFedd] = useState([
-    {id:1, name: "Júlio", idade:32, email: "julio@gmail.com"},
-    {id:2, name: "Henrique", idade:32, email: "julio@gmail.com"},
-    {id:3, name: "Vieira", idade:32, email: "julio@gmail.com"},
-    {id:4, name: "Silva", idade:32, email: "julio@gmail.com"},
+    {id:1, name: "Júlio", age:32, email: "julio@gmail.com"},
+    {id:2, name: "Henrique", age:32, email: "julio@gmail.com"},
+    {id:3, name: "Vieira", age:32, email: "julio@gmail.com"},
+    {id:4, name: "Silva", age:32, email: "julio@gmail.com"},
+    {id:5, name: "Tais Vieira", age:34, email: "julio@gmail.com"},
   ]);
 
-  function UserProfile(){
+  function UserProfile(props){
     return(
-      <View style={styles.profile}>
-        <Text style={styles.description}>Minha lista</Text>
+      <View 
+        style={styles.profile}
+        key={props.data.id}
+      >
+        <Text style={styles.description}>{props.data.name}</Text>
+        <Text style={styles.description}>{props.data.age}</Text>
+        <Text style={styles.description}>{props.data.email}</Text>
       </View>
     ) 
   }
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
       <FlatList
+        keyExtractor={ (item)=> item.id}
         data={feed}
-        renderItem={ ({item}) => <UserProfile/> }
+        renderItem={ ({item}) => <UserProfile data={item}/> }
       />
     </View>
   );
@@ -39,6 +44,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     height: 200,
     marginBottom: 15,
+    justifyContent: "center",
   },
   description:{
     color: "#fff",
